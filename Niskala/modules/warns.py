31746e3c467405f8ff95affc60f9b7e0d729ae8f
BionXP <112658830+BionXP@users.noreply.github.com>
2022-12-17 17:42:29 +0800

    Update warns.py

--- Niskala/modules/warns.py
+++ Niskala/modules/warns.py
@@ -500,7 +500,7 @@ __help__ = """
 ✗ /warnlimit - `<num> set the warning limit`
 ✗ /strongwarn - `<on/yes/off/no> If set to on, exceeding the warn limit will result in a ban. Else, will just punch.`
 
-*Maintained By : @IDnyaAL!*
+*Maintained By : @sowhtido!*
 """
 
 __mod_name__ = "Wᴀʀɴɪɴɢ"
