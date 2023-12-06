1d7715293db0df0348851e09ff95a4661202a3f2
Kadal <87251452+Djangonew@users.noreply.github.com>
2023-12-06 23:51:14 +0700

    Update config.py

--- Niskala/config.py
+++ Niskala/config.py
@@ -68,7 +68,7 @@ class Config(object):
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
-ALLOW_CHATS = None
+ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
 class Production(Config):
     LOGGER = True
